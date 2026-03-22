16b8fd1ddc910b5696122fb3a93f2be437336b38
wndx2 <22222w@duck.com>
2026-03-22 21:35:32 +1300

    fix: github pages error with Quartz

--- quartz.config.ts
+++ quartz.config.ts
@@ -1,16 +1,20 @@
-const config = {
-  pageTitle: "sleepy",
-  baseUrl: "sleepywndud.github.io",
-  description: "NCEA Level 2 and 3 Notes for Mathematics, Statistics, and Calculus.",
-  enableDrafts: false,
-  ignorePatterns: ["private", "Drafts"],
-  theme: {
-    typography: {
-      header: "Schibsted Grotesk",
-      body: "Source Sans Pro",
-      code: "IBM Plex Mono"
-    }
+const config = { 
+  pageTitle: "sleepy", 
+  baseUrl: "sleepywndud.github.io", 
+  description: "NCEA Level 2 and 3 Notes for Mathematics, Statistics, and Calculus.", 
+  enableDrafts: false, 
+  ignorePatterns: ["private", "Drafts"], 
+  theme: { 
+    typography: { 
+      header: "Schibsted Grotesk", 
+      body: "Source Sans Pro", 
+      code: "IBM Plex Mono" 
+    } 
+  },
+  plugins: {
+    emitters: [
+      { name: "HTML" }
+    ]
   }
-}
-
+} 
 export default config
