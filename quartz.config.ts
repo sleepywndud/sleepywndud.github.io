93ae5a60bc113bef265671e3b6f4d978dbae1cd5
wndx2 <w2n2d2@gmail.com>
2026-03-23 00:30:56 +1300

    fix: umami-analytics configurations

--- quartz.config.ts
+++ quartz.config.ts
@@ -1,15 +1,23 @@
-const config = { 
+const config = {
   pageTitle: "🔭",
-  baseUrl: "sleepywndud.github.io", 
-  description: "NCEA Level 2 and 3 Notes for Mathematics, Statistics, and Calculus.", 
-  enableDrafts: false, 
-  ignorePatterns: ["private", "Drafts"], 
-  theme: { 
-    typography: { 
-      header: "Schibsted Grotesk", 
-      body: "Source Sans Pro", 
-      code: "IBM Plex Mono" 
-    } 
-  }
-} 
+  baseUrl: "sleepywndud.github.io",
+  description: "NCEA Level 2 and 3 Notes for Mathematics, Statistics, and Calculus.",
+  enableDrafts: false,
+  ignorePatterns: ["private", "Drafts"],
+
+  theme: {
+    typography: {
+      header: "Schibsted Grotesk",
+      body: "Source Sans Pro",
+      code: "IBM Plex Mono",
+    },
+  },
+
+  analytics: {
+    provider: "umami",
+    host: "https://cloud.umami.is",
+    websiteId: "0a44a84e-faa9-4f02-a1c7-23bfc588c4d8",
+  },
+}
+
 export default config
