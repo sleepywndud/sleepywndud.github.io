522450bbcda6f3105b066256161de2d682062119
wndx2 <22222w@duck.com>
2026-03-25 23:28:46 +1300

    fix(revert): quartz config

---
+++ quartz.config.ts
@@ -0,0 +1,17 @@
+const config = {
+  pageTitle: "🔭",
+  baseUrl: "sleepywndud.github.io",
+  description: "NCEA Level 2 and 3 Notes for Mathematics, Statistics, and Calculus.",
+  enableDrafts: false,
+  ignorePatterns: ["private", "Drafts"],
+
+  theme: {
+    typography: {
+      header: "Schibsted Grotesk",
+      body: "Source Sans Pro",
+      code: "IBM Plex Mono",
+    },
+  },
+}
+
+export default config
