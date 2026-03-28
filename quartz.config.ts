7ef3da3510dfbf7f6a364d0047b3258934ceb66a
wndx2 <22222w@duck.com>
2026-03-28 22:25:25 +1300

    fix(styles): pagetitle change

--- quartz.config.ts
+++ quartz.config.ts
@@ -3,7 +3,7 @@ import * as Plugin from "./quartz/plugins"
 
 const config: QuartzConfig = {
   configuration: {
-    pageTitle: "Quartz 4",
+    pageTitle: "🔭",
     pageTitleSuffix: "",
     enableSPA: true,
     enablePopovers: true,
