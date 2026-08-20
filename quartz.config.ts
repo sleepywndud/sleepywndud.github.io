38d40a6d28ce8b88f52174cb17e0f6e91a83f7ae
wndx2 <w2n2d2@gmail.com>
2026-08-21 10:52:03 +1200

    Update quartz.config.ts
    Co-Authored-By: 주영 <22222w@duck.com>

--- quartz.config.ts
+++ quartz.config.ts
@@ -9,7 +9,7 @@ import * as Plugin from "./quartz/plugins"
 const config: QuartzConfig = {
   configuration: {
     pageTitle: "🎈",
-    pageTitleSuffix: "wndud's Archive",
+    pageTitleSuffix: " ㅣ cai jiu duo lian",
     enableSPA: true,
     enablePopovers: true,
     analytics: {
