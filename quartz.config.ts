12ad18bbb4732dcec04bcd8a051f0c143d7df519
wndx2 <w2n2d2@gmail.com>
2026-08-29 14:51:57 +1200

    Update quartz.config.ts
    Co-Authored-By: 주영 <22222w@duck.com>

--- quartz.config.ts
+++ quartz.config.ts
@@ -3,7 +3,7 @@ import * as Plugin from "./quartz/plugins"
 
 const config: QuartzConfig = {
   configuration: {
-    pageTitle: "・▾・",
+    pageTitle: "NBTS ・▾・",
     // pageTitle: ">⩊<.ᐟ",
     // pageTitle: "˖ ݁♬⋆.˚𝄞",
     // pageTitle: "\\(^o^)/",
