c35a1032c81367196aae343fdb1e5781a9a4876e
wndx2 <22222w@duck.com>
2026-09-07 15:05:13 +1200

    style: change theme to cyan
    Co-Authored-By: Juyoung (James) Park <parkj2@duck.com>

--- quartz.config.ts
+++ quartz.config.ts
@@ -2,7 +2,7 @@ import { QuartzConfig } from "./quartz/cfg"
 import * as Plugin from "./quartz/plugins"
 
 // Base color
-const SECONDARY_DARK = "#5865f2"
+const SECONDARY_DARK = "#58f2e0"
 
 function hexToHsl(hex: string): [number, number, number] {
   const r = parseInt(hex.slice(1, 3), 16) / 255
