3676606352bedc3b27d6a8ec18b6058b8ff152c4
wndx2 <22222w@duck.com>
2026-09-09 12:15:49 +1200

    style: change theme to red
    Co-Authored-By: Juyoung (James) Park <parkj2@duck.com>

--- quartz.config.ts
+++ quartz.config.ts
@@ -1,7 +1,7 @@
 import { QuartzConfig } from "./quartz/cfg"
 import * as Plugin from "./quartz/plugins"
 
-const SECONDARY_DARK = "#ff00ff"
+const SECONDARY_DARK = "#ff5858"
 const MAIN_FONT = "ChosunGu"
 
 function hexToHsl(hex: string): [number, number, number] {
