13f7320231f7efd4ebd8afc7868fa3099a57213d
wndx2 <22222w@duck.com>
2026-09-15 13:27:10 +1200

    feat: change theme to achieved
    Co-Authored-By: Juyoung (James) Park <parkj2@duck.com>

--- quartz.config.ts
+++ quartz.config.ts
@@ -1,7 +1,7 @@
 import { QuartzConfig } from "./quartz/cfg"
 import * as Plugin from "./quartz/plugins"
 
-const SECONDARY_DARK = "#7758ff"
+const SECONDARY_DARK = "#227722"
 const MAIN_FONT = "ChosunGu"
 
 function hexToHsl(hex: string): [number, number, number] {
